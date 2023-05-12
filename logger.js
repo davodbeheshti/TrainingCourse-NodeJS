9110590b1637268b8a4271571cfabc148501fdf7
davodbeheshti <davood.beheshti2018@gmail.com>
2023-05-13 00:57:48 +0330

    create file logger

---
+++ logger.js
@@ -0,0 +1,12 @@
+const text = 'davodBeheshti';
+
+const sayHi = name => {
+    console.log(`hello ${name}`);
+}
+
+module.exports = {
+    sayHi,
+    text
+}
+// exports.text = text;
+// exports.sayHi = sayHi;
