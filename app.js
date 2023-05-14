20f465ab63eada4de8177127288ed864d329da16
davodbeheshti <davood.beheshti2018@gmail.com>
2023-05-14 23:50:23 +0330

    file system sync

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
 
-fs.writeFile('test.txt', 'hello world', (err) => {
-    if (err) throw err
-    console.log("data is saved");
-}) 
+// fs.writeFile('test.txt', 'hello world', (err) => {
+//     if (err) throw err
+//     console.log("data is saved");
+// }) 
+fs.writeFileSync('testSync_fs.txt' , 'hello file system sync')
 
-fs.appendFile('test.txt' , '\nhello davodJonam'  , (err) => {
-    if(err) throw err;
-    console.log('data is appended');
-})
+// fs.appendFile('test.txt' , '\nhello davodJonam'  , (err) => {
+//     if(err) throw err;
+//     console.log('data is appended');
+// })
