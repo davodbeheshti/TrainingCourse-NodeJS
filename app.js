8049ea9e6d81fb469ca38b5892044e042fb38743
davodbeheshti <davood.beheshti2018@gmail.com>
2023-05-14 23:53:21 +0330

    renamed fs

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const fs = require('fs');
 //     if (err) throw err
 //     console.log("data is saved");
 // }) 
-fs.writeFileSync('testSync_fs.txt' , 'hello file system sync')
+// fs.writeFileSync('testSync_fs.txt' , 'hello file system sync')
 
 // fs.appendFile('test.txt' , '\nhello davodJonam'  , (err) => {
 //     if(err) throw err;
 //     console.log('data is appended');
 // })
+
+fs.rename('test.txt' , 'testRenam.txt' , (err) => {
+    if(err) throw err;
+    console.log('data is renamed');
+})
