0c85ca0574a8d226f9aab248f0b3c7274f64ff42
davodbeheshti <davood.beheshti2018@gmail.com>
2023-05-14 23:46:34 +0330

    learning fileSystem (fs)

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log(process.argv);
+const fs = require('fs');
+
+fs.writeFile('test.txt', 'hello world', (err) => {
+    if (err) throw err
+    console.log("data is saved");
+}) 
+
+fs.appendFile('test.txt' , '\nhello davodJonam'  , (err) => {
+    if(err) throw err;
+    console.log('data is appended');
+})
