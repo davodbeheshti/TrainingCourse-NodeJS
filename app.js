858ee80101b6e89678daf2e410e02fdc1b1ee192
davodbeheshti <davood.beheshti2018@gmail.com>
2023-05-17 01:48:21 +0330

    fixed crud project terminal

--- app.js
+++ app.js
@@ -1,31 +1,59 @@
 const yargs = require('yargs');
 
+// 
+const { addContact, removeContact, listContacts } = require('./contacts')
+
 yargs.command({
     command: "create",
-    aliases : ['c' , 'ct'], 
+    aliases: ['c', 'ct'],
     describe: '[create new contact]',
     builder: {
         fullName: {
-            alias : "f",
+            alias: "f",
             describe: 'Person FullName',
             demandOption: true,
-            type : "string"
+            type: "string"
         },
-        phone : {
-            alias : 'p',
-            describe : "Person phone Number",
-            demandOption : true,
-            type : 'number',
+        phone: {
+            alias: 'p',
+            describe: "Person phone Number",
+            demandOption: true,
+            type: 'number',
         },
-        email : {
-            alias : 'e',
-            describe : "Person Email Address",
-            demandOption : true,
-            type : 'string'
+        email: {
+            alias: 'e',
+            describe: "Person Email Address",
+            demandOption: true,
+            type: 'string'
         }
     },
-    handler({fullName , email , phone}) {
-        console.log(fullName, email, phone);
+    handler({ fullName, email, phone }) {
+        addContact(fullName, phone, email);
+    }
+})
+yargs.command({
+    command: "list",
+    aliases: ['l'],
+    describe: '[listing the saved contacts]',
+    handler() {
+        listContacts();
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    aliases: ['r', 'd'],
+    describe: '[remvoe the user]',
+    builder: {
+        fullName: {
+            alias: "f",
+            describe: 'Person FullName',
+            demandOption: true,
+            type: "string"
+        },
+    },
+    handler({ fullName }) {
+        removeContact(fullName);
     }
 })
 
