7a272f5e14adc8d66e8f4908096cb1301ded0658
davodbeheshti <davood.beheshti2018@gmail.com>
2023-05-20 23:58:02 +0330

    used body parser

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
     console.log('this runs everyTime')
