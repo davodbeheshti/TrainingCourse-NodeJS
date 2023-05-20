13c11cb985473c8fedfff449e27a60dd672d13b5
davodbeheshti <davood.beheshti2018@gmail.com>
2023-05-20 23:53:39 +0330

    routing in express

--- app.js
+++ app.js
@@ -4,14 +4,71 @@ const express = require('express');
 
 const app = express();
 
-app.get('/davod', (req , res) => {
-    res.send('hello davod');
-})
-app.get('/', (req , res) => {
-    res.send('hello world');
+app.use((req, res, next) => {
+    console.log('this runs everyTime')
+    next()
 })
 
+app.use('/hello', (req, res, next) => {
+    res.send('<h1>hello world</h1>')
+    // next()
+})
+
+app.use('/message', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/')
+    // next()
+})
+
+app.use("/", (req, res, next) => {
+    res.send(
+        `<center>
+            <form method="POST" action="/message">
+                <input type="text" name="message" />
+                <input type="submit"/>
+            </form>
+        </center>
+        `
+    );
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.get('/davod', (req , res) => {
+//     res.send('hello davod');
+// })
+
+// app.get('/', (req , res) => {
+//     res.send('hello world');
+// })
  // adonisjs - koa - sailsjs
 
 
