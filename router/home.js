9e60fbdcc31b5f1524bc68760a24a9817fc618e9
davodbeheshti <davood.beheshti2018@gmail.com>
2023-05-21 23:54:50 +0330

    path.join and helperFunction

--- router/home.js
+++ router/home.js
@@ -3,14 +3,19 @@ const path = require('path');
 
 const router = express.Router();
 
+const rootDir = require('../utils/path');
+
 
 router.get('/hello', (req, res, next) => {
-    res.send('<h1>hello world</h1>')
+    // res.send('<h1>hello world</h1>')
+    res.sendFile(path.join(rootDir , 'views' , 'hello.html'))
     // next()
 })
 
+
+
 router.get("/", (req, res, next) => {
-    res.sendFile(path.join(__dirname , '../' , 'views' , 'index.html'))
+    res.sendFile(path.join(rootDir , 'views' , 'index.html'))
 })
 
 module.exports = router;
