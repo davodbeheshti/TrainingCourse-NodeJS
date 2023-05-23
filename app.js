9d73349bfed77df8d8bfbb126c2f955de4aef999
davodbeheshti <davood.beheshti2018@gmail.com>
2023-05-23 23:50:06 +0330

    clear code

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
-// const path = require('path');
+const path = require('path');
+const express = require('express');
 
-// const express = require('express');
-// const bodyParser = require('body-parser');
+const app = express();
 
+app.use(express.static(path.join(__dirname , 'public')));
 
-// const adminRoutes = require('./router/admin');
-// const homeRoutes = require('./router/home');
+app.set('view engine' , 'pug');
+app.set("views" , 'views')
 
-// const app = express();
+app.get('/' , (req , res) => {
+    res.send("<h1>hello world</h1>");
+})
 
-// app.use(bodyParser.urlencoded({ extended: false }));
-
-// app.use()
-// app.use('/admin' , adminRoutes);
-// app.use(homeRoutes);
-
-// app.use((req , res) => {
-//     res.sendfile(path.join(__dirname , 'views' , '404.html'));
-// })
-
-// app.listen(3000);
-
-//////////////////////
-
-
-
+app.listen(3000 , () => console.log("server is runing."))
--- http-demo.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-
-const server = http.createServer((req, res) => {
-
-})
-
-server.listen(3000)
