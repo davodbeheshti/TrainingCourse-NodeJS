474d443ca5c1a23b523e027e07e5d001c62cac26
davodbeheshti <davood.beheshti2018@gmail.com>
2023-05-23 23:42:04 +0330

    commneting older code

--- app.js
+++ app.js
@@ -1,53 +1,27 @@
-const path = require('path');
+// const path = require('path');
 
-const express = require('express');
-const bodyParser = require('body-parser');
-
-
-const adminRoutes = require('./router/admin');
-const homeRoutes = require('./router/home');
-
-const app = express();
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(express.static(path.join(__dirname , 'public')))
-app.use('/admin' , adminRoutes);
-app.use(homeRoutes);
-
-app.use((req , res) => {
-    res.sendfile(path.join(__dirname , 'views' , '404.html'));
-})
+// const express = require('express');
+// const bodyParser = require('body-parser');
 
 
+// const adminRoutes = require('./router/admin');
+// const homeRoutes = require('./router/home');
 
+// const app = express();
 
-app.listen(3000);
+// app.use(bodyParser.urlencoded({ extended: false }));
 
+// app.use()
+// app.use('/admin' , adminRoutes);
+// app.use(homeRoutes);
 
-
-
-
-
-
-
-
-
-
-
-
-
-// app.get('/davod', (req , res) => {
-//     res.send('hello davod');
+// app.use((req , res) => {
+//     res.sendfile(path.join(__dirname , 'views' , '404.html'));
 // })
 
-// app.get('/', (req , res) => {
-//     res.send('hello world');
-// })
- // adonisjs - koa - sailsjs
+// app.listen(3000);
+
+//////////////////////
 
 
-//  const server = http.createServer(app);
 
-
-//  server.listen(3000);
