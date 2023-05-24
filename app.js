34cac4678eb6b9ea5fc61e8a6881030601e9a9a8
davodbeheshti <davood.beheshti2018@gmail.com>
2023-05-24 23:07:45 +0330

    working view engin pug

--- app.js
+++ app.js
@@ -9,7 +9,13 @@ app.set('view engine' , 'pug');
 app.set("views" , 'views')
 
 app.get('/' , (req , res) => {
-    res.send("<h1>hello world</h1>");
+    const arr = [
+        {name : 'davod beheshti' , age : 23},
+        {name : 'davod' , age : 24353},
+        {name : 'tara hoseini', age : 21},
+        {name : 'mona yari' , age : 20},
+    ]
+    res.render('index' , {titlePage: "first Page with pug" , text : "hello davod jonam" , names : arr})
 })
 
 app.listen(3000 , () => console.log("server is runing."))
