92cf0123b29c514b229d8cf16883c54c8f4dd065
davodbeheshti <davood.beheshti2018@gmail.com>
2023-05-27 00:43:58 +0330

    handelbars

--- app.js
+++ app.js
@@ -4,22 +4,30 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname , 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // app.set('view engine' , 'pug');
 console.log('sjdlf')
-app.engine('hbs' , exphbs.engine());
-app.set('view engine' , 'hbs');
-app.set("views" , 'views')
+app.engine('hbs', exphbs.engine({
+    extname: "hbs",
+    layoutsDir: "views/layouts",
+    defaultLayout: "main",
+    partialsDir: "views/partials",
+}));
+app.set('view engine', 'hbs');
+app.set("views", 'views')
 
-app.get('/' , (req , res) => {
+app.get('/about', (req, res) => {
+    res.render('index', { titlePage: "about page", text: "hello about page", about: true })
+})
+app.get('/', (req, res) => {
     const arr = [
-        {name : 'davod beheshti' , age : 23},
-        {name : 'davod' , age : 24353},
-        {name : 'tara hoseini', age : 21},
-        {name : 'mona yari' , age : 20},
+        { name: 'davod beheshti', age: 23 },
+        { name: 'davod', age: 24353 },
+        { name: 'tara hoseini', age: 21 },
+        { name: 'mona yari', age: 20 },
     ]
-    res.render('index' , {titlePage: "first Page with pug" , text : "hello davod jonam" , names : arr , layout : false})
+    res.render('index', { titlePage: "first Page with pug", text: "hello davod jonam", names: arr, home: true, hasName: arr.length > 0 })
 })
 
-app.listen(3000 , () => console.log("server is runing."))
+app.listen(3000, () => console.log("server is runing."))
