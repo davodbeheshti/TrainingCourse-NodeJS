4dc79216c6a5e47da8c2fe29a602f385224fdc5e
davodbeheshti <davood.beheshti2018@gmail.com>
2023-05-27 01:32:06 +0330

    ejs

--- app.js
+++ app.js
@@ -7,17 +7,18 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 // app.set('view engine' , 'pug');
-app.engine('hbs', exphbs.engine({
-    extname: "hbs",
-    layoutsDir: "views/layouts",
-    defaultLayout: "main",
-    partialsDir: "views/partials",
-}));
-app.set('view engine', 'hbs');
+// app.engine('hbs', exphbs.engine({
+//     extname: "hbs",
+//     layoutsDir: "views/layouts",
+//     defaultLayout: "main",
+//     partialsDir: "views/partials",
+// }));
+app.set('view engine' , 'ejs');
+// app.set('view engine', 'hbs');
 app.set("views", 'views')
 
 app.get('/about', (req, res) => {
-    res.render('index', { titlePage: "about page", text: "hello about page", about: true })
+    res.render('index', { titlePage: "about page", text: "hello about page", names : []})
 })
 app.get('/', (req, res) => {
     const arr = [
@@ -26,7 +27,7 @@ app.get('/', (req, res) => {
         { name: 'tara hoseini', age: 21 },
         { name: 'mona yari', age: 20 },
     ]
-    res.render('index', { titlePage: "first Page with pug", text: "hello davod jonam", names: arr, home: true, hasName: arr.length > 0 })
+    res.render('index', { titlePage: "first Page with pug", text: "hello davod jonam", names: arr })
 })
 
 app.listen(3000, () => console.log("server is runing."))
