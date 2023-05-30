868ba773d280c829a9439b2ea46e9ab30153b69c
davodbeheshti <davood.beheshti2018@gmail.com>
2023-05-30 23:51:04 +0330

    config todo.js and create file controller

--- models/todo.js
+++ models/todo.js
@@ -1,14 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 
+const rootDir = require('../utils/path');
+
+const filePath = path.join(rootDir, 'data', 'todos.json')
+
 class Todo {
-    constructor(id , text , completed = false) {
+    constructor(id, text, completed = false) {
         this.id = id;
         this.text = text;
         this.completed = completed;
     }
 
     save(callback) {
-        fs.writeFile()
+        // const data = {id , text , completed}
+        fs.writeFile(filePath, JSON.stringify(this), (err) => {
+            if (err) callback(err)
+        })
     }
 }
+module.exports = Todo;
