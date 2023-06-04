29a2d9b434d2267dec2bc926134804f89130c57e
davodbeheshti <davood.beheshti2018@gmail.com>
2023-06-05 00:04:34 +0330

    count completed

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,10 +1,23 @@
 const Todo = require("../models/todo");
+const todoUtils = require('../utils/todos')
 
-exports.getIndex = (req , res) => {
+
+exports.getIndex = (req, res) => {
+    let completedWork = 0;
+    let notCompletedWork = 0;
     Todo.fetchAll((todos) => {
-        res.render("index" , {
+        todos.map(t => {
+            if (t.completed) {
+                completedWork += 1;
+            } else {
+                notCompletedWork += 1;
+            }
+        })
+        res.render("index", {
             pageTitle: "کارهای روزمره",
-            todos
+            todos,
+            completedWork,
+            notCompletedWork
         })
     })
 }
