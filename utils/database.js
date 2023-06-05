04a6f96cd5d4827ddfe181ddc566ca3d8a7dc76d
davodbeheshti <davood.beheshti2018@gmail.com>
2023-06-06 00:32:34 +0330

    config sequelize in database.js

---
+++ utils/database.js
@@ -0,0 +1,8 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize("todo_db" , "root" , "ab@!NO" , {
+    dialect : 'mysql',
+    host : "localhost"
+});
+
+module.exports = sequelize;
