d68a1ed7effd924d9ee7d1187ffe9da4bc0f8479
davodbeheshti <davood.beheshti2018@gmail.com>
2023-06-07 00:20:11 +0330

    async and await in getIndex

--- controllers/todos.js
+++ controllers/todos.js
@@ -2,16 +2,24 @@ const Todo = require("../models/todo");
 const todoUtils = require('../utils/todos')
 
 
-exports.getIndex = (req, res) => {
-    Todo.count({ where: { completed: true } }).then((completedTodos) => {
-        Todo.findAll().then(todos => {
-            res.render("index", {
-                pageTitle: "کارهای روزمره",
-                todos,
-                completedTodos,
-                notcompletedTodos: todos.length - completedTodos
-            })
-        })
+exports.getIndex = async (req, res) => {
+    // Todo.count({ where: { completed: true } }).then((completedTodos) => {
+    //     Todo.findAll().then(todos => {
+    //         res.render("index", {
+    //             pageTitle: "کارهای روزمره",
+    //             todos,
+    //             completedTodos,
+    //             notcompletedTodos: todos.length - completedTodos
+    //         })
+    //     })
+    // })
+    const completedTodos = await Todo.count({ where: { completed: true } });
+    const todos = await Todo.findAll();
+    res.render("index", {
+        pageTitle: "کارهای روزمره",
+        todos,
+        completedTodos,
+        notcompletedTodos: todos.length - completedTodos
     })
 }
 
