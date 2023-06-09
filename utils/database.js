383888c5d103a3ce55438e5a4d89ec4e0d6b69a6
davodbeheshti <davood.beheshti2018@gmail.com>
2023-06-10 00:36:50 +0330

    clean code sequelize from dataBase.js in utils

--- utils/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize("todo_db" , "root" , "ab@!NO" , {
-    dialect : 'mysql',
-    host : "localhost"
-});
-
-module.exports = sequelize;
