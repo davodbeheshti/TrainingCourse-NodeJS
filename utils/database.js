e1ec3c8654e1fe4fb2616f8c58608d28667417d9
davodbeheshti <davood.beheshti2018@gmail.com>
2023-06-10 01:17:01 +0330

    config database

--- utils/database.js
+++ utils/database.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/todo_db', {})
+mongoose.connect('mongodb://localhost/todo_db', {
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+})
 .then(() => console.log("connected tod database"))
 .catch((err) => console.log(err))
