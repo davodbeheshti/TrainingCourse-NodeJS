ba6aee99f3a1e6881407c3fc5898f9debbe6af72
davodbeheshti <davood.beheshti2018@gmail.com>
2023-06-12 00:38:50 +0330

    end of cookie

--- app.js
+++ app.js
@@ -7,6 +7,21 @@ const app = express();
 // custom middleware
 app.use(cookieParser());
 
+// routes
+app.get('/', (req, res) => {
+    console.log(res.cookies);
+    res.cookie("name", 'express',
+        {
+            // expires: new Date(Date.now() + 600000),
+            maxAge: 600000
+        }
+    ).send("Cookie has been saved!")
+})
+
+app.get("/delete_cookie", (req, res) => {
+    res.clearCookie("name").send("Cookie Deleted !");
+})
+
 
 
 app.listen(3000, () => console.log('server is running on port 3000'))
