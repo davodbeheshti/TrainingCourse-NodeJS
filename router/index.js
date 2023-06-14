2abb1b206df70acea399e70e1b17e4b5c2669c95
davodbeheshti <davood.beheshti2018@gmail.com>
2023-06-14 23:20:34 +0330

    config router get

--- router/index.js
+++ router/index.js
@@ -2,4 +2,8 @@ const { Router } = require('express');
 
 const router = new Router();
 
+router.get("/", (req, res) => {
+    res.send("index", { pageTitle: 'وبلاگ' })
+})
+
 module.exports.router = router;
