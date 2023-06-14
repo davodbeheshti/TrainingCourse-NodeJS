b49a22e03c9b28aad34db898e81c88e07fcd18e2
davodbeheshti <davood.beheshti2018@gmail.com>
2023-06-14 23:22:32 +0330

    using the router custome

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const path = require('path');
 
 const express = require('express');
+const indexRoutes = require('./router/index')
 
 const app = express();
 
 // view engine
-app.set('view engine' , 'ejs');
-app.set('views' , 'views')
+app.set('view engine', 'ejs');
+app.set('views', 'views')
 
 // static folder
 app.set(express.static(path.join(__dirname, 'public')))
 
-
+// routes
+app.use(indexRoutes)
 app.listen(3000, () => console.log('server is runnint on port 3000'));
