de9e68b672e4c557eb9c1a50666ced9b6e8110f0
davodbeheshti <davood.beheshti2018@gmail.com>
2023-06-14 23:16:24 +0330

    set static files

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 
 const app = express();
@@ -5,6 +7,9 @@ const app = express();
 // view engine
 app.set('view engine' , 'ejs');
 app.set('views' , 'views')
+
+// static folder
+app.set(express.static(path.join(__dirname, 'public')))
 
 
 app.listen(3000, () => console.log('server is runnint on port 3000'));
