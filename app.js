d0dc1e076a75465f28cf844192cd16f4ba35808d
davodbeheshti <davood.beheshti2018@gmail.com>
2023-06-14 23:14:23 +0330

    set view engins

--- app.js
+++ app.js
@@ -2,4 +2,9 @@ const express = require('express');
 
 const app = express();
 
+// view engine
+app.set('view engine' , 'ejs');
+app.set('views' , 'views')
+
+
 app.listen(3000, () => console.log('server is runnint on port 3000'));
