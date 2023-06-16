2350f00c75490222f4c353d17d03c96f55c76684
davodbeheshti <davood.beheshti2018@gmail.com>
2023-06-16 23:52:31 +0330

    require morgan in app

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const dotEnv = require('dotenv');
+const morgan = require('morgan');
 
 const indexRoutes = require('./routes')
 
