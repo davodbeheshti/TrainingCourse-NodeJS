df20678fadaa6d1edf1e40b6cc0644413093665c
davodbeheshti <davood.beheshti2018@gmail.com>
2023-06-18 00:39:43 +0330

    changes route layout

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const { Router } = require('express');
 const router = new Router();
 
 router.get("/", (req, res) => {
-    res.render("index", { pageTitle: 'وبلاگ' })
+    res.render("index", { pageTitle: 'وبلاگ' , layout : "./layouts/loginLayout" });
 })
 
 module.exports = router;
