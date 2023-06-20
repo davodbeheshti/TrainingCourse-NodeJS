73ec1d624c5c7bb7642a5fb9f1c397d3c8314d23
davodbeheshti <davood.beheshti2018@gmail.com>
2023-06-21 00:05:14 +0330

    remve layouts

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const { Router } = require('express');
 const router = new Router();
 
 router.get("/", (req, res) => {
-    res.render("index", { pageTitle: 'وبلاگ' , layout : "./layouts/loginLayout" });
+    res.render("index", { pageTitle: 'وبلاگ' });
 })
 
 module.exports = router;
