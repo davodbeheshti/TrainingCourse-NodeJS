3a14f2b52f6ea2dcf6c2538d47a538d1ded1d9e7
davodbeheshti <davood.beheshti2018@gmail.com>
2023-06-21 23:56:59 +0330

    adding route dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,6 +2,11 @@ const { Router } = require("express");
 
 const router = new Router();
 
+router.get('/' , (req , res) => {
+    res.render('dashboard', {pageTitle : "بخش مدیریت | داشبورد" , path : "/dashboard" , layout : "./layouts/dashLayout"})
+})
+
+
 router.get('/login', (req, res) => {
     res.render('login', { pageTitle: "ورود به بخش مدیریت", path: '/login' })
 })
