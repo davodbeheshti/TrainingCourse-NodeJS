878cfdd1154dd219486d311681597594fa685a6e
davodbeheshti <davood.beheshti2018@gmail.com>
2023-06-24 00:53:15 +0330

    fixed model user

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const { data } = require('jquery');
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+
+    fullname: {
+        type: String,
+        require: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        require: true,
+        uniq: true,
+    },
+    password: {
+        type : String,
+        require: true,
+        minlength : 4,
+        maxlenght: 255
+    },
+    createdAt : {
+        type : Date,
+        default : Date.now
+    }
+
+})
+
+const User = mongoose.model('User' , UserSchema)
+
+module.exports = User;
