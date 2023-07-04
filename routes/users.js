3e0b67ff91f2db38dc2e0c168500465f86b798dc
davodbeheshti <davood.beheshti2018@gmail.com>
2023-07-04 23:53:14 +0330

    commenting code validator yup

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,24 @@
 const { Router } = require('express');
-const Yup = require('yup');
+// const Yup = require('yup');
 
 const router = new Router();
 
-const schema = Yup.object().shape({
-    fullname: Yup.string()
-        .required("نام و نام خانوادگی الزامی می باشد")
-        .min(4, "نام و نام خانوادگی نباید کمتر از 4 کاراکتر باشد")
-        .max(255, "نام و نام خانوادگی نباید بیشتر از 255 کاراکتر باشد"),
-    email: Yup.string()
-        .email("ایمیل معتبر نمی باشد")
-        .required("ایمیل الزامی می باشد"),
-    password: Yup.string()
-        .min(4, "کلمه عبور نباید کمتر از 4 کاراکتر باشد")
-        .max(255, "کلمه عبور نباید بیشتر از 255 کاراکتر باشد")
-        .required("کلمه عبور الزامی می باشد"),
-    confirmPassword: Yup.string()
-        .required("تکرار کلمه عبور الزامی می باشد")
-        .oneOf([Yup.ref("password"), null]),
-});
+// const schema = Yup.object().shape({
+//     fullname: Yup.string()
+//         .required("نام و نام خانوادگی الزامی می باشد")
+//         .min(4, "نام و نام خانوادگی نباید کمتر از 4 کاراکتر باشد")
+//         .max(255, "نام و نام خانوادگی نباید بیشتر از 255 کاراکتر باشد"),
+//     email: Yup.string()
+//         .email("ایمیل معتبر نمی باشد")
+//         .required("ایمیل الزامی می باشد"),
+//     password: Yup.string()
+//         .min(4, "کلمه عبور نباید کمتر از 4 کاراکتر باشد")
+//         .max(255, "کلمه عبور نباید بیشتر از 255 کاراکتر باشد")
+//         .required("کلمه عبور الزامی می باشد"),
+//     confirmPassword: Yup.string()
+//         .required("تکرار کلمه عبور الزامی می باشد")
+//         .oneOf([Yup.ref("password"), null]),
+// });
 
 router.get('/login', (req, res) => {
     res.render('login', { pageTitle: "ورود به بخش مدیریت", path: "/login" })
@@ -38,12 +38,12 @@ router.post('/register', (req, res) => {
     //     res.send('err ')
     // })
 
-    schema.validate(req.body)
-        .then((result) => {
-            res.redirect("/users/login");
-        }).catch(err => {
-            res.render("register", { pageTitle: "ثبت نام کاربر", path: "/register", errors: err.errors });
-        })
+    // schema.validate(req.body)
+    //     .then((result) => {
+    //         res.redirect("/users/login");
+    //     }).catch(err => {
+    //         res.render("register", { pageTitle: "ثبت نام کاربر", path: "/register", errors: err.errors });
+    //     })
 })
 
 
