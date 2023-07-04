47ba579de3c39b5bb5c272219af102072f293c6a
davodbeheshti <davood.beheshti2018@gmail.com>
2023-07-05 00:26:32 +0330

    check the valid the route post

--- routes/users.js
+++ routes/users.js
@@ -60,7 +60,28 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
+    const validate = v.validate(req.body , schema);
+    const errorArr = [];
 
+    if(validate === true) {
+        const { fullname , email , password , confirmPassword } = req.body;
+        if(password !== confirmPassword) {
+            errorArr.push({message : "کلمه عبور یکسان نیست !!"})
+             
+            return res.render("register" , {
+                pageTitle : "ثبت نام کاربر",
+                path : "/register",
+                errors : errorArr
+            })
+        }
+        res.redirect("/users/login");
+    }else {
+        res.render("register" , {
+            pageTitle : "ثبت نام کاربر",
+            path : "/register",
+            errors : validate
+        })
+    }
 })
 
 
