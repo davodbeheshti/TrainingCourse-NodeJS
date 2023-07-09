ecbc9faba1e94ba9afe20fc588b7f4189f2cd1a3
davodbeheshti <davood.beheshti2018@gmail.com>
2023-07-09 23:45:16 +0330

    add property schema

--- models/user.js
+++ models/user.js
@@ -1,35 +1,50 @@
-const { data } = require('jquery');
-const mongoose = require('mongoose');
-const  Yap = require('yap');
+const mongoose = require("mongoose");
+const Yup = require("yup");
 
 const userSchema = new mongoose.Schema({
-
     fullname: {
         type: String,
-        require: [true, "نام و نام خانوادگی الزامی میباشد"],
-        trim: true
+        required: [true, "نام و نام خانوادگی الزامی می باشد"],
+        trim: true,
     },
     email: {
         type: String,
-        require: true,
-        uniq: true,
+        required: true,
+        unique: true,
     },
     password: {
         type: String,
-        require: true,
+        required: true,
         minlength: 4,
-        maxlenght: 255
+        maxlength: 255,
     },
     createdAt: {
         type: Date,
-        default: Date.now
-    }
+        default: Date.now,
+    },
+});
 
-})
+const schema = Yup.object().shape({
+    fullname: Yup.string()
+        .required("نام و نام خانوادگی الزامی می باشد")
+        .min(4, "نام و نام خانوادگی نباید کمتر از 4 کاراکتر باشد")
+        .max(255, "نام و نام خانوادگی نباید بیشتر از 255 کاراکتر باشد"),
+    email: Yup.string()
+        .email("ایمیل معتبر نمی باشد")
+        .required("ایمیل الزامی می باشد"),
+    password: Yup.string()
+        .min(4, "کلمه عبور نباید کمتر از 4 کاراکتر باشد")
+        .max(255, "کلمه عبور نباید بیشتر از 255 کاراکتر باشد")
+        .required("کلمه عبور الزامی می باشد"),
+    confirmPassword: Yup.string()
+        .required("تکرار کلمه عبور الزامی می باشد")
+        .oneOf([Yup.ref("password"), null]),
+});
 
-const schema = 
+userSchema.statics.userValidation = function (body) {
+    return schema.validate(body, { abortEarly: false });
+};
 
+const User = mongoose.model("User", userSchema);
 
-const User = mongoose.model('User', userSchema)
-
-module.exports = User; 
+module.exports = User;
