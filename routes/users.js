652f76c38e34f2925ffacafe64c9fca448ba9832
davodbeheshti <davood.beheshti2018@gmail.com>
2023-07-10 23:48:56 +0330

    show message

--- routes/users.js
+++ routes/users.js
@@ -5,15 +5,29 @@ const User = require('../models/user');
 const router = new Router();
 
 router.get('/login', (req, res) => {
-    res.render('login', { pageTitle: "ورود به بخش مدیریت", path: "/login" }) 
+    res.render('login', { pageTitle: "ورود به بخش مدیریت", path: "/login" })
 })
 
 router.get('/register', (req, res) => {
     res.render('register', { pageTitle: "ثبت نام کاربر", path: "/register" })
 })
 
-router.post('/register', (req, res) => {
-    
+router.post('/register', async (req, res) => {
+    try {
+        await User.userValidation(req.body);
+        req.redirect('/users/login')
+    } catch (err) {
+        console.log(err);
+        const errors = [];
+        err.inner.forEach(e => {
+            errors.push({
+                name: e.path,
+                message: e.message
+            })
+        });
+
+        return res.render('register', { pageTitle: "ثبت نام کاربر", path: '/register', errors })
+    }
 })
 
 
