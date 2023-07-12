435efa9f60ead94996586391cada7d704c198b2e
davodbeheshti <davood.beheshti2018@gmail.com>
2023-07-13 01:30:42 +0330

    hashing password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+
 const User = require("../models/user")
 
 exports.login = (req, res) => {
@@ -8,19 +10,29 @@ exports.register = (req, res) => {
     res.render('register', { pageTitle: "ثبت نام کاربر", path: "/register" })
 }
 
-
 exports.createUser = async (req, res) => {
     const errors = [];
     try {
         await User.userValidation(req.body);
-        const { fullName, email, password } = req.body;
+        const { fullname, email, password } = req.body;
         const user = await User.findOne({ email });
         if (user) {
             errors.push({ message: "کاربری با این ایمیل موجود است." });
             return res.render('register', { pageTitle: "ثبت نام کاربر", path: '/register', errors });
         }
-        await User.create(req.body)
-        res.redirect('/users/login')
+        bcrypt.genSalt(10, (err, salt) => {
+            if (err) throw err;
+            bcrypt.hash(password, salt, async (err, hash) => {
+                if (err) throw err;
+                await User.create({
+                    fullname,
+                    email,
+                    password: hash
+                })
+            })
+            res.redirect('/users/login')
+        })
+        // await User.create(req.body)
     } catch (err) {
         console.log("---******---", err);
         err.inner.forEach(e => {
