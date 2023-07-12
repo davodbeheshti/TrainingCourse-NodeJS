b7a2da165a188a1d650c64b6fddb27f80189a539
davodbeheshti <davood.beheshti2018@gmail.com>
2023-07-13 01:48:46 +0330

    config middleware flash in app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const session = require('express-session');
 const connectDB = require('./config/db');
 
 /// load config
-dotEnv.config({path : './config/config.env'});
+dotEnv.config({ path: './config/config.env' });
 
 // dataBase connection
 connectDB();
@@ -18,28 +18,40 @@ connectDB();
 const app = express();
 
 // logging
-if(process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "development") {
     app.use(morgan("dev"));
 }
 
 // view engine
 app.use(expressLayout);
 app.set('view engine', 'ejs');
-app.set('layout' , './layouts/mainLayout');
+app.set('layout', './layouts/mainLayout');
 app.set('views', 'views');
 
 //body parser
-app.use(express.urlencoded({extended : false}));
+app.use(express.urlencoded({ extended: false }));
+
+// session
+app.use(session({
+    secret: 'secret',
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: false
+}));
+
+// flash
+app.use(flash());
+
 // static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
 // routes
-app.use("/" , require('./routes/blog'));
-app.use('/users' , require('./routes/users'))  
-app.use('/dashboard' , require('./routes/dashboard'));
-app.use((req , res) => {
-    res.render('404' , {pageTitle: "صفحه پیدا نشد | 404"  , path : "/404"})
-}) 
+app.use("/", require('./routes/blog'));
+app.use('/users', require('./routes/users'))
+app.use('/dashboard', require('./routes/dashboard'));
+app.use((req, res) => {
+    res.render('404', { pageTitle: "صفحه پیدا نشد | 404", path: "/404" })
+})
 
 const PORT = process.env.PORT || 3000;
 
