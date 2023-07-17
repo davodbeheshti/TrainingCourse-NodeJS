9393fea48494116171957c51ca7523f2bab42879
davodbeheshti <davood.beheshti2018@gmail.com>
2023-07-18 01:03:55 +0330

    clean the code

--- config/winston.js
+++ config/winston.js
@@ -2,18 +2,18 @@ const winston = require('winston');
 const appRoot = require('app-root-path');
 
 const options = {
-    File : {
-        level : 'info',
-        filename : `${appRoot}/logs/app.log`,
+    File: {
+        level: 'info',
+        filename: `${appRoot}/logs/app.log`,
         handleExceptions: true,
-        format  :winston.format.json(),
-        maxsize : 5000000,
-        maxFile : 5
+        format: winston.format.json(),
+        maxsize: 5000000,
+        maxFile: 5
     },
-    console : {
-        level : "debug",
-        handleExceptions : true,
-        format : winston.format.combine(
+    console: {
+        level: "debug",
+        handleExceptions: true,
+        format: winston.format.combine(
             winston.format.colorize(),
             winston.format.simple()
         )
@@ -21,15 +21,15 @@ const options = {
 }
 
 const logger = new winston.createLogger({
-    transports : [
+    transports: [
         new winston.transport.File(options.File),
         new winston.transport.Console(options.console),
     ],
-    exitOnError : false
+    exitOnError: false
 })
 
 logger.stream = {
-    write : function (message) {
+    write: function (message) {
         logger.info(message)
     }
 }
